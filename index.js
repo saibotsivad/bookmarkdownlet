624ee5f4e44ce1e5a9a57c21e766196655807212
Tobias Davis <accounts@davistobias.com>
2020-11-07 00:48:39 -0600

    this is probably closer to what i would want

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const { description, version } = require('./package.json')
 const { randomBytes } = require('crypto')
 const sade = require('sade')
-const server = require('./server.js')
+const server = require('./lib/server.js')
 
 const DEFAULT_PORT = 53074
 
@@ -11,16 +11,15 @@ const generateSecret = () => randomBytes(32)
 	.toString('base64')
 	.replace(/[+=/]/g, '')
 
-sade('bookmarkdownlet', true)
+sade('bookmarkdownlet <output_path>', true)
 	.version(version)
 	.describe(description)
-	.option('--output, -o', 'The path to write the extracted Markdown files.')
 	.option('--port, -p', `The port to use when clicking the browser bookmarklet. [Default: ${DEFAULT_PORT}]`)
 	.option('--secret, -s', 'Secret code required to authenticate your bookmarklet. [Default: printed at startup]')
 	.option('--transform, -t', 'Optional path to a JavaScript file used to modify final output.')
-	.action(async ({ output, port, secret, transform }) => {
+	.action(async (output, { port, secret, transform }) => {
 		port = port || DEFAULT_PORT
 		secret = secret || generateSecret()
-		server({ port, secret, transform })
+		server({ output, port, secret, transform })
 	})
 	.parse(process.argv)
