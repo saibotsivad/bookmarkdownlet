9c18bea0df69f8bd89c451a13bf04915e618679c
Tobias Davis <accounts@davistobias.com>
2020-11-06 22:58:50 -0600

    something along these lines for the bookmarklet and cli tool

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 
 const { description, version } = require('./package.json')
+const { randomBytes } = require('crypto')
 const sade = require('sade')
+const server = require('./server.js')
+
+const DEFAULT_PORT = 53074
+
+const generateSecret = () => randomBytes(32)
+	.toString('base64')
+	.replace(/[+=/]/g, '')
 
 sade('bookmarkdownlet', true)
 	.version(version)
 	.describe(description)
 	.option('--output, -o', 'The path to write the extracted Markdown files.')
-	.option('--port, -p', 'The port to use when clicking the browser bookmarklet. [Default: 53074]')
+	.option('--port, -p', `The port to use when clicking the browser bookmarklet. [Default: ${DEFAULT_PORT}]`)
+	.option('--secret, -s', 'Secret code required to authenticate your bookmarklet. [Default: printed at startup]')
 	.option('--transform, -t', 'Optional path to a JavaScript file used to modify final output.')
-	.action(async ({ output, port, transform }) => {
-		console.log('--------', output, port, transform)
+	.action(async ({ output, port, secret, transform }) => {
+		port = port || DEFAULT_PORT
+		secret = secret || generateSecret()
+		server({ port, secret, transform })
 	})
 	.parse(process.argv)
